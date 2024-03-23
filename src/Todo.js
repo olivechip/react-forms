53b543cfe08898abb9213c27feb693ac0fd3dfbf
Oliver <oliver@MSI>
2024-03-22 22:01:32 -0700

    added Todo component and deleteTodo function

---
+++ src/Todo.js
@@ -0,0 +1,7 @@
+const Todo = ({ todo, deleteTodo }) => {
+    return (
+        <li>{todo}<button onClick={() => deleteTodo(todo)}>X</button></li>
+    )
+}
+
+export default Todo;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,8 +1,24 @@
 import { useState } from "react";
+import Todo from "./Todo";
 
 const TodoList = () => {
+    const INITIAL_STATE = ["cut grass", "feed dog"];
+    const [ todoList, setTodoList ] = useState(INITIAL_STATE);
+
+    const deleteTodo = (itemRemove) => {
+        setTodoList(todoList => {
+            return todoList.filter(todo => todo !== itemRemove);
+        })
+    }
     return (
-        <div>Hi</div>
+        <div>
+            <h3>Todo List</h3>
+            <ul>
+                {todoList.map((todo, index) => (
+                    <Todo key={index} todo={todo} deleteTodo={deleteTodo}/>
+                ))}
+            </ul>
+        </div>
     )
 }
 
