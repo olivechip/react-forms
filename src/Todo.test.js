884a6e2b9219ab069f879c3e55527dd89d5e2d99
Oliver <oliver@MSI>
2024-03-23 08:25:21 -0700

    added tests (3) for all todos

---
+++ src/Todo.test.js
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react';
+import Todo from './Todo';
+
+it('should render', () => {
+  render(<Todo />);
+});
+
+it('should match snapshot', () => {
+  const {asFragment} = render(<Todo />);
+  expect(asFragment()).toMatchSnapshot();
+});
---
+++ src/TodoList.test.js
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react';
+import TodoList from './TodoList';
+
+it('should render', () => {
+  render(<TodoList />);
+});
+
+it('should match snapshot', () => {
+  const {asFragment} = render(<TodoList />);
+  expect(asFragment()).toMatchSnapshot();
+});
